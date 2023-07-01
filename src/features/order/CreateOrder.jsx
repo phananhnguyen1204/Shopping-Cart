bea7bea5f83da37ac42741c360bd0aa0acd0983e
Phan Anh <phananh12042003@gmail.com>
2023-07-01 01:40:15 -0400

    Model cart "state" 🚀

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
@@ -38,6 +39,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const formErros = useActionData();
+  const username = useSelector((state) => state.user.username);
 
   return (
     <div className="px-4 py-6">
@@ -46,7 +48,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
   return (
-    <div className="hidden text-sm font-semibold md:block">Phan Anh Nguyen</div>
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
 }
 
