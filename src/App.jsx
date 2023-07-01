d9879e30004fe76ec7a92f6bfa18c83289651587
Phan Anh <phananh12042003@gmail.com>
2023-06-30 21:50:19 -0400

    add files

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout></AppLayout>,
+    //this error will replace the AppLayOut component
+    // errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/menu",
+        element: <Menu></Menu>,
+        loader: menuLoader,
+        errorElement: <Error></Error>,
+      },
+      {
+        path: "/cart",
+        element: <Cart></Cart>,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder></CreateOrder>,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:orderID",
+        element: <Order></Order>,
+        loader: orderLoader,
+        errorElement: <Error></Error>,
+      },
+    ],
+  },
+]);
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,7 @@
+function Username() {
+  return (
+    <div className="hidden text-sm font-semibold md:block">Phan Anh Nguyen</div>
+  );
+}
+
+export default Username;
